8b7b1b333660867efb2fda433c72a622ae2cf21d
sommai.k@gmail.com <sommai.k@gmail.com>
2021-10-21 03:32:09 +0000

    re format code

--- src/main/java/com/business/auth/security/SecurityCredentialsConfig.java
+++ src/main/java/com/business/auth/security/SecurityCredentialsConfig.java
@@ -1,4 +1,5 @@
 package com.business.auth.security;
+
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,43 +16,49 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EnableWebSecurity
 public class SecurityCredentialsConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private UserDetailsService userDetailsService;
+	@Autowired
+	private UserDetailsService userDetailsService;
 
-    private String _uri = "/auth/**";
-    
-    @Override
+	private String _uri = "/auth/**";
+
+	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http
-		    .csrf().disable()
-		     // make sure we use stateless session; session won't be used to store user's state.
-	            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-	        .and()
-	            // handle an authorized attempts 
-	            .exceptionHandling().authenticationEntryPoint((req, rsp, e) -> rsp.sendError(HttpServletResponse.SC_UNAUTHORIZED))
-	        .and()
-		    // Add a filter to validate user credentials and add token in the response header
-			
-		    // What's the authenticationManager()? 
-		    // An object provided by WebSecurityConfigurerAdapter, used to authenticate the user passing user's credentials
-		    // The filter needs this auth manager to authenticate the user.
-		    .addFilter(new JwtUsernameAndPasswordAuthenticationFilter(authenticationManager()))	
-		.authorizeRequests()
-			.antMatchers(HttpMethod.POST, _uri).permitAll()
-			.antMatchers("/user/**").permitAll()
-		    .anyRequest().authenticated();
+		http.csrf().disable()
+				// make sure we use stateless session; session won't be used to store user's
+				// state.
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+				// handle an authorized attempts
+				.exceptionHandling()
+				.authenticationEntryPoint((req, rsp, e) -> rsp.sendError(HttpServletResponse.SC_UNAUTHORIZED)).and()
+				// Add a filter to validate user credentials and add token in the response
+				// header
+
+				// What's the authenticationManager()?
+				// An object provided by WebSecurityConfigurerAdapter, used to authenticate the
+				// user passing user's credentials
+				// The filter needs this auth manager to authenticate the user.
+				.addFilter(new JwtUsernameAndPasswordAuthenticationFilter(authenticationManager())).authorizeRequests()
+				// auth url
+				.antMatchers(HttpMethod.POST, _uri).permitAll()
+				// user url
+				.antMatchers("/user/**").permitAll()
+				// all url
+				.anyRequest().authenticated();
 	}
-	
-	// Spring has UserDetailsService interface, which can be overriden to provide our implementation for fetching user from database (or any other source).
-	// The UserDetailsService object is used by the auth manager to load the user from database.
-	// In addition, we need to define the password encoder also. So, auth manager can compare and verify passwords.
+
+	// Spring has UserDetailsService interface, which can be overriden to provide
+	// our implementation for fetching user from database (or any other source).
+	// The UserDetailsService object is used by the auth manager to load the user
+	// from database.
+	// In addition, we need to define the password encoder also. So, auth manager
+	// can compare and verify passwords.
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
 	}
-	
+
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
-	    return new BCryptPasswordEncoder();
+		return new BCryptPasswordEncoder();
 	}
 }
